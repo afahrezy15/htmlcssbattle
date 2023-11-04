c63cd6c8f099fdb3eb9adeaf770e199c971af1bf
afahrezy15 <rezy.sanny@gmail.com>
2023-11-04 15:09:54 +0700

    fixing cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const registerRoutes = require("./routes/registerRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const materiRoutes = require("./routes/materiRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(registerRoutes);
 app.use(profileRoutes);
 app.use(materiRoutes);
